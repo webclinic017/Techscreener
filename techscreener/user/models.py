59ed40dfe9ea36345484e96a9e6803cd7d48af5a
= <coding.ayush@gmail.com>
2021-10-20 12:23:01 +0530

    country added to user table

--- techscreener/user/models.py
+++ techscreener/user/models.py
@@ -5,6 +5,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, default="India")
     is_admin = models.BooleanField(default=False),
     username = None
 
